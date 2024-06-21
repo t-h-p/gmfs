8bc4818dae2aa453dd03161b447486d52081c0e2
tom <thomashoytepickett@gmail.com>
2024-06-20 21:32:52 -0700

    Added some of command line argument parsing, graph Node struct.

--- TODO.md
+++
@@ -1,8 +0,0 @@
-gmfs (graph my filesystem)
-
-start from current (or specified if -d flag) directory -- make a fun graph of entire filesystem from there
-
-uses -- libgvc (graphviz), dirent.h to get directories and such
-
-
-
--- src/main.c
+++ src/main.c
@@ -1,8 +1,36 @@
 #include<stdio.h>
 #include<dirent.h>
 #include<graphviz/gvc.h>
+#include<sys/stat.h>
+
+typedef struct Node {
+    char* name;
+    bool is_dir;
+    struct node *children[];
+} Node_t;
+
+struct Args_s {
+    bool dir_only;
+    char* path_name;
+} Args_default = {false, "."}; /* Initialize as "Args mInstance = Args_default;"*/
+
+typedef struct Args_s Args;
 
 int main (int argc, char *argv[]) {
-    printf("This is a test.. do not be alarmed\n");
+
+    Args mArgs = Args_default;
+
+    for (int i = 0; i < argc; i++) {
+        if (argv[i][0] == '-') { /* if option */
+            switch (argv[i][1]) {
+                case 'd':
+                    mArgs.dir_only = true;
+            }
+        } else {
+            for (int j = 0; argv[i][j] != '\0'; j++) {
+                mArgs.path_name += argv[i][j];
+            }
+        }
+    }
     return 0;
 }
